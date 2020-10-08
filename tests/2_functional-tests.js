a3efc2e83fa917e26a54e598586d78902c17a06f
SaintPeter <rex.schrader@gmail.com>
2020-10-08 09:59:58 -0700

    Add `chai-http`

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,7 +7,11 @@
  */
 
 const chai = require('chai');
+const chaiHttp = require('chai-http');
 const assert = chai.assert;
+const server = require('../server.js');
+
+chai.use(chaiHttp);
 
 let Translator = require('../components/translator.js');
 
