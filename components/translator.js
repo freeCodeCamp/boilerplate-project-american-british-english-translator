d21a996c1d186fc57b4d5443a067042215532eef
Rex Schrader <rex.schrader@gmail.com>
2020-11-07 11:29:58 -0800

    Feature: Refactor American/British Translator Project Boilerplate for a Backend Focus to Improve Testability (#6)
    * Update Packages, ES6 Syntax
    
    * Initial Relayout
    
    * Update User Stories, Complete Front-end Boilerplate, Update Tests
    
    * Import Language Modules, Fix module export
    
    * Add `chai-http`
    
    * Remove Notes
    
    * Remove User Stories
    
    * Reformat README
    
    * Add Favicon
    
    * Fix time and fruit issues
    
    * Update routes/api.js
    
    Co-authored-by: Randell Dawson <5313213+RandellDawson@users.noreply.github.com>
    
    Co-authored-by: Randell Dawson <5313213+RandellDawson@users.noreply.github.com>

---
+++ components/translator.js
@@ -0,0 +1,10 @@
+const americanOnly = require('./american-only.js');
+const americanToBritishSpelling = require('./american-to-british-spelling.js');
+const americanToBritishTitles = require("./american-to-british-titles.js")
+const britishOnly = require('./british-only.js')
+
+class Translator {
+
+}
+
+module.exports = Translator;
---
+++ routes/api.js
@@ -0,0 +1,21 @@
+/*
+*
+*
+*       Complete the API routing below
+*
+*
+*/
+
+'use strict';
+
+const Translator = require('../components/translator.js');
+
+module.exports = function (app) {
+  
+  const translator = new Translator();
+
+  app.route('/api/translate')
+    .post((req, res) => {
+      
+    });
+};
