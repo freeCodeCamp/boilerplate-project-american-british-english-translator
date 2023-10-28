a751bd1589fb45329202ee2d0543fd8d0bcb10b8
Eli <eli.serra173@gmail.com>
2023-10-28 18:33:22 +0200

    Add enums with locale enum class

---
+++ utils/enums.js
@@ -0,0 +1,8 @@
+const LocaleLang = Object.freeze({
+    AMERICAN_TO_BRITISH:   Symbol("american-to-british"),
+    BRITISH_TO_AMERICAN:  Symbol("british-to-american")
+  });
+
+module.exports = {
+    LocaleLang
+};
