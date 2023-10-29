77d9fa9fc83e1c22a77dad309663ff0013142d6d
Gordian <68512769+Gordiancyber@users.noreply.github.com>
2023-10-29 08:17:03 +0100

    commit comments to Update index.js

--- public/index.js
+++ public/index.js
@@ -1,14 +1,17 @@
-
+// Function to handle translation
 const translateHandler = async () => {
+  // Get references to HTML elements
   const textArea = document.getElementById("text-input");
   const localeArea = document.getElementById("locale-select");
   const errorArea = document.getElementById("error-msg");
   const translatedArea = document.getElementById("translated-sentence");
-  
-  const stuff = {"text": textArea.value, "locale": localeArea.value};
+
+  // Prepare data for translation
+  const stuff = { "text": textArea.value, "locale": localeArea.value };
   errorArea.innerText = "";
   translatedArea.innerText = "";
 
+  // Send a POST request to the translation API
   const data = await fetch("/api/translate", {
     method: "POST",
     headers: {
@@ -18,14 +21,17 @@ const translateHandler = async () => {
     body: JSON.stringify(stuff)
   });
 
+  // Parse the response data as JSON
   const parsed = await data.json();
   if (parsed.error) {
     errorArea.innerText = JSON.stringify(parsed);
     return;
   }
 
+  // Display the translated text
   translatedArea.innerHTML = parsed.translation;
   return;
 };
 
-document.getElementById("translate-btn").addEventListener("click", translateHandler)
+// Add a click event listener to the translate button
+document.getElementById("translate-btn").addEventListener("click", translateHandler);
